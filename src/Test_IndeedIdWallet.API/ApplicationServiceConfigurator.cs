37a4a66693b1d962ae4e10308a7f7e23f6ffdbe7
Shefer Alexander <alexander@typemock.com>
2020-03-20 22:28:48 +0300

    - repositories for users and wallets were registered

--- src/Test_IndeedIdWallet.API/ApplicationServiceConfigurator.cs
+++ src/Test_IndeedIdWallet.API/ApplicationServiceConfigurator.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Test_IndeedIdWallet.Core.Entities;
 using Test_IndeedIdWallet.Core.Services;
 using Test_IndeedIdWallet.Core.Services.Interfaces;
+using Test_IndeedIdWallet.Infrastructure.Repositories;
 
 namespace Test_IndeedIdWallet.API
 {
@@ -16,8 +18,19 @@ namespace Test_IndeedIdWallet.API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Services
+
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IWalletService, WalletService>();
+
+            #endregion
+
+            #region Repositories
+
+            services.AddScoped<IRepository<AppUser>, DbRepository<AppUser>>();
+            services.AddScoped<IRepository<Wallet>, DbRepository<Wallet>>();
+
+            #endregion
         }
     }
 }
