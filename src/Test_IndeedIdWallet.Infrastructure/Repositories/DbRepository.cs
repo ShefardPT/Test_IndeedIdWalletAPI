c7022adcdc88bdae458568141a031c1794fa12c0
Shefer Alexander <alexander@typemock.com>
2020-03-20 22:26:39 +0300

    Repositories were implemented

---
+++ src/Test_IndeedIdWallet.Infrastructure/Repositories/DbRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test_IndeedIdWallet.Infrastructure.Repositories
+{
+    public class DbRepository<T> : IRepository<T> where T : class
+    {
+        public IQueryable<T> Data { get; }
+        private DbSet<T> _table;
+        private ApplicationDbContext _ctx;
+
+        public DbRepository(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+            _table = _ctx.Set<T>();
+            Data = _table.AsTracking();
+        }
+
+        public IQueryable<T> Get(Expression<Func<T, bool>> filter = null)
+        {
+            var result = Data.AsNoTracking();
+
+            if (filter != null)
+            {
+                result = result.Where(filter);
+            }
+
+            return result;
+        }
+
+        public T Add(T item)
+        {
+            _table.Add(item);
+            _ctx.SaveChanges();
+            return item;
+        }
+
+        public async Task<T> AddAsync(T item)
+        {
+            await _table.AddAsync(item);
+            await _ctx.SaveChangesAsync();
+            return item;
+        }
+
+        public T Update(T item)
+        {
+            _table.Update(item);
+            _ctx.SaveChanges();
+            return item;
+        }
+
+        public async Task<T> UpdateAsync(T item)
+        {
+            _table.Update(item);
+            await _ctx.SaveChangesAsync();
+            return item;
+        }
+
+        public void Delete(T item)
+        {
+            _table.Remove(item);
+            _ctx.SaveChanges();
+
+        }
+
+        public async Task DeleteAsync(T item)
+        {
+            _table.Remove(item);
+            await _ctx.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Test_IndeedIdWallet.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_IndeedIdWallet.Infrastructure.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> Data { get; }
+
+        IQueryable<T> Get(Expression<Func<T, bool>> filter);
+        T Add(T item);
+        Task<T> AddAsync(T item);
+        T Update(T item);
+        Task<T> UpdateAsync(T item);
+        void Delete(T item);
+        Task DeleteAsync(T item);
+    }
+}
