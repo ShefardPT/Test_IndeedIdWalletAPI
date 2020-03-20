6f753ebcff9f540c2628985378d62501215f0172
Shefer Alexander <alexander@typemock.com>
2020-03-20 19:03:03 +0300

    Startup.ConfigureServices was modified,  services were configured,
    - ApplicationDbContext was registered,
    - Special class ApplicationServiceConfigurator was added to register services,

---
+++ src/Test_IndeedIdWallet.API/ApplicationServiceConfigurator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Test_IndeedIdWallet.API
+{
+    public class ApplicationServiceConfigurator
+    {
+        public ApplicationServiceConfigurator()
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+
+        }
+    }
+}
